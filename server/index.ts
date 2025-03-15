3cfd82ec4fd75670439d02165c2c048f4432feda
zett-8 <kame0008@gmail.com>
2025-03-15 19:36:06 +0100

    fix

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,8 @@
 import { Hono } from 'hono'
-import { setHandlers } from './handlers'
 import { setMiddlewares } from './middlewares'
 
 const app = new Hono<HonoENV>()
 
 setMiddlewares(app)
-setHandlers(app)
 
 export default app

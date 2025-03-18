b7ba68f4fe6d02bf71247eab2594acdcab44fcd5
zett-8 <kame0008@gmail.com>
2025-03-18 22:33:47 +0100

    compress not needed on workers

--- server/middlewares.ts
+++ server/middlewares.ts
@@ -1,7 +1,6 @@
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { drizzle } from 'drizzle-orm/d1'
 import type { Hono } from 'hono'
-import { compress } from 'hono/compress'
 import { contextStorage } from 'hono/context-storage'
 import { createFactory } from 'hono/factory'
 import { logger } from 'hono/logger'
@@ -11,12 +10,6 @@ import * as schema from '../database/schema/index'
 export const setMiddlewares = (app: Hono<HonoENV>): Hono<HonoENV> => {
   app.use(appendTrailingSlash())
   app.use(logger())
-  app.use(async (c, next) => {
-    if (c.env.TEST) {
-      return next()
-    }
-    return compress({ encoding: 'deflate' })(c, next)
-  })
   app.use(contextStorage())
 
   app.use(async (c, next) => {
@@ -52,13 +45,13 @@ const setAuthForDashboard = F.createMiddleware(async (c, next) => {
   return next()
 })
 
-const setAuthForAPI = F.createMiddleware(async (_, next) => {
-  // const auth = getAuth(c)
+const setAuthForAPI = F.createMiddleware(async (c, next) => {
+  const auth = getAuth(c)
 
-  // if (auth && auth.userId) {
-  //   c.set('userId', auth.userId)
-  //   return next()
-  // }
+  if (auth && auth.userId) {
+    c.set('userId', auth.userId)
+    return next()
+  }
 
   // do some other auth logic for API
   return next()
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,12 +1,30 @@
 // @ts-expect-error vitest pool workers provide a cloudflare:test module AT RUNTIME
 // eslint-disable-next-line
 import { env } from 'cloudflare:test'
-import { compress } from 'hono/compress'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll, vi, afterAll } from 'vitest'
 import app from '../server'
+import { setTestEnv } from './utils'
 
 describe('GET /api/check', () => {
-  app.use(compress({ encoding: 'gzip' }))
+  let testEnv
+
+  beforeAll(() => {
+    testEnv = setTestEnv(env)
+
+    vi.mock('@hono/clerk-auth', () => ({
+      clerkMiddleware: vi.fn().mockImplementation(
+        () =>
+          function (c, next) {
+            return next()
+          }
+      ),
+      getAuth: vi.fn().mockImplementation(() => ({})),
+    }))
+  })
+
+  afterAll(() => {
+    vi.resetAllMocks()
+  })
 
   it('Should return 200', async () => {
     const res = await app.request(
@@ -17,7 +35,7 @@ describe('GET /api/check', () => {
           'Content-Type': 'application/json',
         },
       },
-      { ...env, TEST: true }
+      testEnv
     )
 
     expect(res.status).toBe(200)

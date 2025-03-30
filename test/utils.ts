7f01f58ef9be91a4851c9353dbb46aef14b6aba9
zett-8 <kame0008@gmail.com>
2025-03-30 21:23:57 +0200

    update mini branch

--- test/utils.ts
+++
@@ -1,6 +0,0 @@
-export const setTestEnv = (env) => ({
-  ...env,
-  VITE_CLERK_PUBLISHABLE_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  CLERK_PUBLISHABLE_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  CLERK_SECRET_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-})
